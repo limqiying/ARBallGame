8fc09e6052cc868829aff5fb56fdd3507214da47
Tia Lim <limqiying19@gmail.com>
2019-11-11 01:56:39 -0500

    initial commit

---
+++ Assets/ThrowBall.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThrowBall : MonoBehaviour
+{
+    public Camera firstPersonCamera;
+    Touch touch;
+
+    void Update()
+    {
+        if (Input.touchCount < 1 || (touch = Input.GetTouch(0)).phase != TouchPhase.Began)
+        {
+            return;
+        }
+
+        GameObject ball = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+        ball.transform.position = firstPersonCamera.transform.TransformPoint(0, 0, 0.5f);
+        ball.transform.localScale = new Vector3(0.2f, 0.2f, 0.2f);
+        ball.AddComponent<Rigidbody>();
+        ball.GetComponent<MeshRenderer>().material.color = Random.ColorHSV();
+        ball.GetComponent<Rigidbody>().AddForce(firstPersonCamera.transform.TransformDirection(0, 1f, 2f), ForceMode.Impulse);
+
+    }
+}
