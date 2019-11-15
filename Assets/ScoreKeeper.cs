626c8c7ad57416d50cf833c925bb9f19acbf04f1
Tia Lim <limqiying19@gmail.com>
2019-11-14 19:58:25 -0500

    add trigger collider

---
+++ Assets/ScoreKeeper.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreKeeper : MonoBehaviour
+{
+	public int startScore;
+
+    void Start()
+	{
+		startScore = 0;
+	}
+
+    public void AddOnePoint()
+	{
+		startScore++;
+	}
+}
---
+++ Assets/ScoreTrigger.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreTrigger : MonoBehaviour
+{
+
+    public ScoreKeeper scoreKeeper;
+   void OnTriggerEnter(Collider other)
+	{
+		scoreKeeper.AddOnePoint();
+	}
+}
