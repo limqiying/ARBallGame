26f2315e745f0b72a69f4860520705d75cdd9a08
Tia Lim <limqiying19@gmail.com>
2019-11-14 20:57:21 -0500

    random sphere color

--- Assets/Scripts/ThrowBall.cs
+++ Assets/Scripts/ThrowBall.cs
@@ -6,6 +6,13 @@ public class ThrowBall : MonoBehaviour
 {
     public Camera firstPersonCamera;
     public GameObject ballPrefab;
+    public List<Material> materials;
+    public System.Random random;
+
+    private void Start()
+    {
+        random = new System.Random();
+    }
 
     public void ThrowButtonPressed()
     {
@@ -13,6 +20,7 @@ public class ThrowBall : MonoBehaviour
         ball.transform.position = firstPersonCamera.transform.TransformPoint(0, 0, 0.5f);
         ball.transform.localScale = new Vector3(0.2f, 0.2f, 0.2f);
         ball.GetComponent<SphereController>().delay = 3.0f;
+        ball.GetComponent<Renderer>().material = materials[random.Next(0, 5)];
         ball.GetComponent<Rigidbody>().AddForce(firstPersonCamera.transform.TransformDirection(0, 1f, 2f), ForceMode.Impulse);
     }
 
