4a50bf91cda53fef36c684d273d0c0b6d4f544ab
Tia Lim <limqiying19@gmail.com>
2019-11-21 22:42:24 -0500

    Playing and setup modes

---
+++ Assets/Scripts/ModeText.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class ModeText : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameMode gameMode;
+    public Text modeText;
+
+    // Update is called once per frame
+    void Update()
+    {
+        modeText.text = gameMode.CurrentMode == Mode.SetUp ? "Play <--" : "Set Up -->";
+    }
+}
