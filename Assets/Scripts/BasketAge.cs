f44d77a3a01e5dc80a371be40edfd23081a9dfd2
Tia Lim <limqiying19@gmail.com>
2019-11-21 22:43:25 -0500

    age tracker for each basket

---
+++ Assets/Scripts/BasketAge.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasketAge : MonoBehaviour
+{
+    private int _age;
+    public int Age
+    {
+        get
+        {
+            return _age;
+        }
+
+        set
+        {
+            _age = value;
+        }
+    }
+
+    private void Start()
+    {
+        _age = 0;
+    }
+}
