4d28faa577b6c5076f005604b220c1651800ff65
Tia Lim <limqiying19@gmail.com>
2019-11-21 22:42:54 -0500

    prevent overlapping before placement

---
+++ Assets/Scripts/TrashController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrashController : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Trash")
+        {
+            int otherAge = other.gameObject.GetComponent<BasketAge>().Age;
+            int myAge = gameObject.GetComponent<BasketAge>().Age;
+            if (myAge < otherAge)
+            {
+                gameObject.SetActive(false);
+                Destroy(gameObject);
+            }
+            else
+            {
+                other.gameObject.SetActive(false);
+                Destroy(other.gameObject);
+            }
+        }
+    }
+}
