9cf4ad3cd3ead90f6642aca284c175442dfb2bef
Tia Lim <limqiying19@gmail.com>
2019-12-02 16:18:31 -0500

    gyroscope input to check gravity direction for placement of world objects

---
+++ Assets/GyroController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GyroController : MonoBehaviour
+{
+    private Gyroscope gyro;
+
+    public bool GyroEnabled { get; set; }
+
+    void Start()
+    {
+        GyroEnabled = EnableGyro();
+    }
+
+    // Update is called once per frame
+    public Vector3 GetGravityDirection()
+    {
+        if (GyroEnabled)
+        {
+            return gyro.gravity;
+        }
+        return new Vector3();
+    }
+
+    private bool EnableGyro()
+    {
+        if (SystemInfo.supportsGyroscope)
+        {
+            gyro = Input.gyro;
+            gyro.enabled = true;
+            return true;
+        }
+        return false;
+    }
+}
